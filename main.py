fa45e146421219d4ac485cd14ab9ba05435613e5
Omkar Dandekar <41143602+techsavvyomi@users.noreply.github.com>
2025-07-01 19:34:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# bike_energy_app/main.py
-from PyQt5.QtWidgets import QApplication
-import sys
-from ui import EnergyApp
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = EnergyApp()
-    window.show()
-    sys.exit(app.exec_())
+    import tkinter as tk
+    from ui import EnergyApp  # assuming your Tkinter app is saved in a file named energy_app_tk.py
+
+    root = tk.Tk()
+    app = EnergyApp(root)
+    root.mainloop()
